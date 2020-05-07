790ba90479a0f9dd59d1d41294c9d0004568b108
exuanbo <exuanbo@protonmail.com>
2020-05-07 15:39:25 +0800

    refactor: rm async iife

--- index.js
+++ index.js
@@ -11,30 +11,28 @@ const PLUGIN_NAME = '@exuanbo/gulp-inline-source'
  * @param {Object} [options] - https://github.com/popeindustries/inline-source#usage
  */
 const gulpInlineSource = (options = {}) => {
-  return through.obj((file, encoding, callback) => {
-    if (file.isNull()) {
-      return callback(null, file)
-    }
-
-    if (file.isStream()) {
-      return callback(new PluginError(PLUGIN_NAME, 'Streaming not supported'))
-    }
-
-    const defaultOptions = {
-      htmlpath: file.path
-    }
-
-    const pluginOptions = { ...defaultOptions, ...options }
-
-    ;(async () => {
-      try {
-        const html = await inlineSource(String(file.contents), pluginOptions)
-        file.contents = Buffer.from(html)
-        callback(null, file)
-      } catch (err) {
-        return callback(new PluginError(PLUGIN_NAME, err))
+  return through.obj(async (file, encoding, callback) => {
+    try {
+      if (file.isNull()) {
+        return callback(null, file)
       }
-    })()
+
+      if (file.isStream()) {
+        return callback(new PluginError(PLUGIN_NAME, 'Streaming not supported'))
+      }
+
+      const defaultOptions = {
+        htmlpath: file.path
+      }
+
+      const pluginOptions = { ...defaultOptions, ...options }
+
+      const html = await inlineSource(String(file.contents), pluginOptions)
+      file.contents = Buffer.from(html)
+      callback(null, file)
+    } catch (err) {
+      return callback(new PluginError(PLUGIN_NAME, err))
+    }
   })
 }
 
