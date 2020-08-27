fcfc72d00abf641e31b5e4db2aeb0975fc4f7244
exuanbo <exuanbo@protonmail.com>
2020-08-27 17:31:16 +0800

    feat: add es module

---
+++ index.es.js
@@ -0,0 +1,46 @@
+import require$$0 from 'inline-source';
+import PluginError from 'plugin-error';
+import through from 'through2';
+
+const { inlineSource } = require$$0;
+
+
+
+const PLUGIN_NAME = '@exuanbo/gulp-inline-source';
+
+/**
+ * inline-source wrapper
+ * @param {Object} [options] - https://github.com/popeindustries/inline-source#usage
+ */
+const gulpInlineSource = (options = {}) => {
+  return through.obj(async (file, encoding, callback) => {
+    try {
+      if (file.isNull()) {
+        return callback(null, file)
+      }
+
+      if (file.isStream()) {
+        return callback(new PluginError(PLUGIN_NAME, 'Streaming not supported'))
+      }
+
+      const defaultOptions = {
+        rootpath: file.base
+      };
+
+      const pluginOptions = { ...defaultOptions, ...options };
+
+      const newFileContents = await inlineSource(
+        String(file.contents),
+        pluginOptions
+      );
+      file.contents = Buffer.from(newFileContents);
+      callback(null, file);
+    } catch (err) {
+      callback(new PluginError(PLUGIN_NAME, err));
+    }
+  })
+};
+
+var gulpInlineSource_1 = gulpInlineSource;
+
+export default gulpInlineSource_1;
